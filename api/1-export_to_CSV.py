87eb7a5dc93eb4a37fec7338f0fd9ac5e1913960
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-16 23:54:53 -0500

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -28,13 +28,13 @@ def api_todo():
     """
     This function accepts an integer as a parameter, which is the employee ID
     """
-    url_base = "https://jsonplaceholder.typicode.com/"
-    url_users = "{}users/{}".format(url_base, argv[1])
+    url_base = "https://jsonplaceholder.typicode.com"
+    url_users = "{}/users/{}".format(url_base, argv[1])
     data_users = get(url_users)
     json_users = data_users.json()
     user_name = json_users.get('username')
 
-    url_todos = "{}todos?userId={}".format(url_base, argv[1])
+    url_todos = "{}/todos?userId={}".format(url_base, argv[1])
     data_todos = get(url_todos)
     json_todos = data_todos.json()
     done_tasks = []
@@ -45,7 +45,8 @@ def api_todo():
 
     csv_file = argv[1] + ".csv"
     with open(csv_file, mode="w", encoding="utf-8") as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
 
         for task in done_tasks:
             writer.writerow(task)
