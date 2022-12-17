be70a96ffe986aabfbe27951624cf8d3f8126050
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-16 23:26:49 -0500

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -29,7 +29,7 @@ def api_todo():
     This function accepts an integer as a parameter, which is the employee ID
     """
     url_base = "https://jsonplaceholder.typicode.com/"
-    url_users = "{}user/{}".format(url_base, argv[1])
+    url_users = "{}users/{}".format(url_base, argv[1])
     data_users = get(url_users)
     json_users = data_users.json()
     user_name = json_users.get('username')
@@ -42,7 +42,7 @@ def api_todo():
     for tasks in json_todos:
         done_tasks.append([argv[1], user_name, tasks.get("completed"),
                            tasks.get("title")])
-    
+
     csv_file = argv[1] + ".csv"
 
     with open(csv_file, mode="w", encoding="utf-8") as f:
