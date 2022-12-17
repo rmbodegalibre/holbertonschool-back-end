45cb24f4c5b1be01b0cc8008eb50b2d59c659fc8
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-17 07:48:30 -0500

    1-export_to_CSV.py 06

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-
 """This module defines an API that saved data of a given employee"""
-
-import requests
-import sys
+from requests import get
+from sys import argv
+import csv
 
 
 def api_todo():
-    us_id = int(sys.argv[1])
-    user_data = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(us_id)).json()
-    user_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            us_id
-        )).json()
+    """
+    This function accepts an integer as a parameter, which is the employee ID
+    """
+    # us_id = sys.argv[1]
+    url_base = "https://jsonplaceholder.typicode.com"
+    url_users = "{}/users/{}".format(url_base, argv[1])
+    # url_users = "https://jsonplaceholder.typicode.com/users?id=" + argv[1]
+    data_users = get(url_users)
+    json_users = data_users.json()
+    user_name = json_users.get("username")
 
-    with open('{}.csv'.format(us_id), 'w') as f:
-        for todo in user_todos:
-            f.write('"{}","{}","{}","{}"\n'.format(
-                us_id,
-                user_data['username'],
-                todo['completed'],
-                todo['title']
-            ))
+    url_todos = "{}/todos?userId={}".format(url_base, argv[1])
+    # url_todos = "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1]
+    data_users = get(url_todos)
+    json_todos = data_users.json()
+    done_tasks = []
+
+    for tasks in json_todos:
+        done_tasks.append([argv[1], user_name, tasks.get("completed"),
+                           tasks.get("title")])
+
+    csv_file = argv[1] + ".csv"
+    with open(csv_file, mode="w", encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for task in done_tasks:
+            writer.writerow(task)
+
 
 if __name__ == "__main__":
     api_todo()
