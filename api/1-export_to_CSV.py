a84434d8ca42dbebe2744806baee6380d704fcee
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-16 23:21:05 -0500

    api/1-export_to_CSV.py

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+This script for a given employee ID, returns information about
+his/her TODO list progress, and exports data in the CSV format.
+
+
+Requirements:
+
+You must use urllib or requests module
+The script must accept an integer as a parameter, which is the employee ID
+The script must display on the standard output the employee TODO list progress
+in this exact format:
+First line: Employee EMPLOYEE_NAME is done with
+tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+EMPLOYEE_NAME: name of the employee
+NUMBER_OF_DONE_TASKS: number of completed tasks
+TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed
+and non-completed tasks
+Second and N next lines display the title of completed tasks: TASK_TITLE
+(with 1 tabulation and 1 space before the TASK_TITLE)
+"""
+from requests import get
+from sys import argv
+import csv
+
+
+def api_todo():
+    """
+    This function accepts an integer as a parameter, which is the employee ID
+    """
+    url_base = "https://jsonplaceholder.typicode.com/"
+    url_users = "{}user/{}".format(url_base, argv[1])
+    data_users = get(url_users)
+    json_users = data_users.json()
+    user_name = json_users.get('username')
+
+    url_todos = "{}todos?userId={}".format(url_base, argv[1])
+    data_todos = get(url_todos)
+    json_todos = data_todos.json()
+    done_tasks = []
+
+    for tasks in json_todos:
+        done_tasks.append([argv[1], user_name, tasks.get("completed"),
+                           tasks.get("title")])
+    
+    csv_file = argv[1] + ".csv"
+
+    with open(csv_file, mode="w", encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for tasks in done_tasks:
+            writer.writerow(tasks)
+
+if __name__ == "__main__":
+    api_todo()
